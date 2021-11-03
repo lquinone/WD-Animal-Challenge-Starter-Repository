c21d2484c440fe678c9180eaa2fe44abf24340b1
lquinone <lizquinone@gmail.com>
2021-11-03 18:34:02 -0400

    first commit

--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -3,6 +3,31 @@ const router = express.Router();
 const { User } = require("../models");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const { UniqueConstraintError } = require("sequelize/types");
+
+router.post("/register", async (req, res) => {
+    let {email, password } = req.body.user;
+    try {
+        const User = await UserModel.create({
+            email,
+            password,
+        });
+
+        res.status(201).json({
+            message: "User has created an account",
+        });
+    }catch (err) {
+        if (err instanceof UniqueConstraintError) {
+            res.status(409).json({
+                message: "Email already in use",
+            });
+        }else {
+            res.status(500).json({
+                message: "Failed to register user",
+            });
+        }
+    }
+})
 
 
 module.exports = router;
