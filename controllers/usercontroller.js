726984483047f525f12bb0f0679ef14d36eee662
lquinone <lizquinone@gmail.com>
2021-11-04 17:36:07 -0400

    Gold Commit

--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -9,23 +9,32 @@ const { UniqueConstraintError } = require("sequelize/lib/errors");
 router.post("/create", async (req, res) => {
 
     let { username, password } = req.body.user;
-    console.log(username, password);
     try {
         const newUser = await User.create({
             username,
-            password,
+            password: bcrypt.hashSync(password, 13),
         });
+
+        let token = jwt.sign({id: newUser.id, username: newUser.username}, process.env.JWT_SECRET,{expiresIn: 60 * 60 * 24});
 
         res.status(201).json({
             message: "User created",
             user: newUser,
+            sessionToken: token
         });
     } catch (err) {
-        res.status(500).json({
-            message: "Failed to create user",
-        });
+        if (err instanceof UniqueConstraintError) {
+            res.status(409).json({
+                message: "Username already in use",
+            });
+        } else {
+            res.status(500).json({
+                message: "Failed to create user",
+            });
+        }
     }
 });
+ 
 
 //Silver Challenge
 router.post("/login", async (req, res) => {
@@ -38,10 +47,19 @@ router.post("/login", async (req, res) => {
                 username: username,
             },
         });
-        res.status(200).json({
+        if (loginUser) {
+            let token = jwt.sign({id: loginUser.id}, process.env.JWT_SECRET, { expiresIn: 60 * 60 * 2 });
+        
+            res.status(200).json({
             user: loginUser,
-            message: "User successfully logged in!"
+            message: "User successfully logged in!",
+            sessionToken: token
         });
+    }else {
+        res.status(401).json({
+            message: 'Login failed'
+        });
+    }
     } catch (error) {
         res.status(500).json({
             message: "Failed to log user in"
